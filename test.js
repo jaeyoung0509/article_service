c81b41f4bf575ca582586cf69cd4c82937a7b8f0
Sean <tnsals9603@naver.com>
2020-12-07 16:58:35 +0900

    .

--- test.js
+++ test.js
@@ -4,3 +4,5 @@ mongoose.connect('mongodb://developer:developer@207.148.99.250:27017/article_ser
 useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
   .then(() => console.log('Successfully connected to mongodb!'))
   .catch(e => console.error(e));
+
+  //
